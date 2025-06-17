42a296c99717428b7fcbd4ed0017a9f2fef2c85e
Shuan920402 <st980155@gmail.com>
2025-06-17 14:21:04 +0800

    2025/06/17 homepage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -6,63 +6,68 @@ import { useAppState } from './AppContext';
 import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
-    const { appState, setAppState } = useAppState();
-    const navigate = useNavigate();
+  const { appState, setAppState } = useAppState();
+  const navigate = useNavigate();
 
-    const connectWallet = async () => {
-        if (!window.ethereum) {
-            alert('請安裝 MetaMask');
-            return;
-        }
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert('請安裝 MetaMask');
+      return;
+    }
 
-        try {
-            const provider = new Web3Provider(window.ethereum);
-            await provider.send('eth_requestAccounts', []);
-            const signer = provider.getSigner();
-            const address = await signer.getAddress();
-            const network = await provider.getNetwork();
-            const did = getEthrDID(address, network.chainId);
-            setAppState(prev => ({
-                ...prev,
-                account: address,
-                did: did
-            }));
-        } catch (err) {
-            console.error('連接失敗:', err);
-        }
-    };
+    try {
+      const provider = new Web3Provider(window.ethereum);
+      await provider.send('eth_requestAccounts', []);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      const network = await provider.getNetwork();
+      const did = getEthrDID(address, network.chainId);
+      setAppState(prev => ({
+        ...prev,
+        account: address,
+        did: did
+      }));
+    } catch (err) {
+      console.error('連接失敗:', err);
+    }
+  };
 
-    useEffect(() => {
-        if (appState.account) {
-            navigate("/", { replace: true });
-        }
-    }, [appState]);
+  useEffect(() => {
+    if (appState.account) {
+      navigate("/", { replace: true });
+    }
+  }, [appState]);
 
-    return (
-        <Container maxWidth="xl" sx={{ marginTop: "15px" }}>
-            <Grid container spacing={2} alignItems='center' justifyContent='center' sx={{ width: "100%" }}>
-                <Grid item xs={12}>
-                    <Typography variant='h4' gutterBottom sx={{ textAlign: 'center' }}>
-                        LuganoChainLab - VC Demo
-                    </Typography>
-                </Grid>
-                <Grid span={12} display="flex" flexDirection="column" alignItems="center">
-                    <Box
-                        component="img"
-                        src={`/LuganoChainLab/homepage.png`}
-                        alt="封面圖片"
-                        sx={{
-                        maxHeight: 300,
-                        objectFit: "contain",
-                        marginBottom: 2
-                        }}
-                    />
-                    <Button variant="contained" onClick={connectWallet}>
-                        使用 MetaMask 登入
-                    </Button>
-                </Grid>
+  return (
+    <Container maxWidth="md" sx={{ marginTop: "40px" }}>
+      <Grid container direction="column" alignItems="center" spacing={3}>
+        {/* 標題 */}
+        <Grid item>
+          <Typography variant="h4" component="h1" align="center">
+            LuganoChainLab - VC Demo
+          </Typography>
+        </Grid>
 
-            </Grid>
-        </Container>
-    );
+        {/* 圖片 */}
+        <Grid item>
+          <Box
+            component="img"
+            src={`/LuganoChainLab/homepage.png`}
+            alt="封面圖片"
+            sx={{
+              maxHeight: 300,
+              objectFit: "contain",
+            }}
+          />
+        </Grid>
+
+        {/* 登入按鈕 */}
+        <Grid item>
+          <Button variant="contained" onClick={connectWallet}>
+            使用 MetaMask 登入
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
